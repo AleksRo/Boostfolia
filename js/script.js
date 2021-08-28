2bb679fdc75f2b8a513924893a5ba45d26213e4b
AleksRo <67026178+AleksRo@users.noreply.github.com>
2021-08-28 20:58:51 +0300

    well done

--- js/script.js
+++ js/script.js
@@ -1,15 +1,24 @@
 document.addEventListener("DOMContentLoaded", function (event) {
-    console.log("DOM fully loaded and parsed");
-    $('slider-btn')
+  console.log("DOM fully loaded and parsed");
+  $('slider-btn')
 
-    const swiper = new Swiper('.swiper', {
-        pagination: {
-          el: '.slider_row',
-          type: 'bullets',
-          bulletClass: 'slider-btn',
-          clickable: 'true'
-        },
-      });
-      setTimeout(() => {$('#exampleModal').modal('show')}, 1000);
-      
-    });
+  const swiper = new Swiper('.swiper', {
+    pagination: {
+      el: '.slider_row',
+      type: 'bullets',
+      bulletClass: 'slider-btn',
+      clickable: 'true'
+    },
+  });
+  setTimeout(() => {
+    $('#exampleModal').modal('show');
+    $('main, header').css('opacity', 0);
+  }, 1000);
+  
+  $('.btn-primary').click(() => { 
+    $('main, header').css('opacity', 1)
+    $('#exampleModal').modal('hide') });
+  $('.modal').modal({
+    backdrop: false
+  })
+});
